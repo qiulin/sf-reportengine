a27f038c53385630a0393c6e4ea8a783fc8790a5
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2013-01-17 17:36:11 +0000

    freemarker child classes added

---
+++ src/main/java/net/sf/reportengine/out/ConfigurableHtmlOutput.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package net.sf.reportengine.out;
+
+/**
+ * Configurable Html output ( based on freemarker templates).
+ * 
+ * The templates that you can configure are : 
+ * 
+ * <ol>
+ * 		<li>startReport.ftl</li>
+ * 		<li>startRow.ftl</li>
+ *  	<li>cell.ftl</li>
+ *  	<li>endRow.ftl</li>
+ *  	<li>endReport.ftl</li>
+ * </ol>
+ *  
+ * @author dragos balan
+ * @since 0.7
+ */
+public class ConfigurableHtmlOutput extends FreemarkerOutput {
+	
+}
---
+++ src/main/java/net/sf/reportengine/out/PdfOutput.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package net.sf.reportengine.out;
+
+/**
+ * Pdf output
+ * 
+ * 
+ * @author dragos balan
+ * @since 0.7
+ */
+public class PdfOutput extends FreemarkerOutput {
+	
+}
