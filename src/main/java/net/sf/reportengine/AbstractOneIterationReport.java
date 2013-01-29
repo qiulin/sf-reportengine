22e3918237a2470da270fc07d1d2be6a9529c5cf
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2013-01-29 13:17:27 +0000

    javadoc improvements

--- src/main/java/net/sf/reportengine/AbstractOneIterationReport.java
+++ src/main/java/net/sf/reportengine/AbstractOneIterationReport.java
@@ -7,8 +7,11 @@ import net.sf.reportengine.core.algorithm.IReportAlgorithm;
 import net.sf.reportengine.core.algorithm.OneIterationAlgorithm;
 
 /**
+ * abstract parent class for reports relying on one single iteration over the provided data
+ * 
  * @author dragos balan (dragos dot balan at gmail dot com)
- *
+ * @since 0.4
+ * @see FlatReport
  */
 public abstract class AbstractOneIterationReport extends AbstractReport {
 	
--- src/main/java/net/sf/reportengine/out/AbstractCharacterOutput.java
+++ src/main/java/net/sf/reportengine/out/AbstractCharacterOutput.java
@@ -10,8 +10,31 @@ import java.io.Writer;
 import net.sf.reportengine.util.ReportIoUtils;
 
 /**
- * @author dragos balan
- *
+ * <p>
+ * abstract parent class for all your character based output.
+ * </p>
+ * 
+ * <p>
+ * What is a character based output? An output backed by a {@link java.io.Writer} to do the writing.<br/> 
+ * Examples of character based outputs: text output, an html output, an xml also
+ * </p>
+ * 
+ * <p>
+ * 	How to extend this class ? <br/>
+ *  This class has basic functionality for handling the opening/closing of the writer.
+ *  Your subclasses should implement the methods :
+ *  <ol> 
+ *  	<li>{@link IReportOutput#output(CellProps)}</li>
+ *  	<li>{@link IReportOutput#startRow(RowProps)} - for row counting use super.startRow()</li>
+ *  	<li>{@link IReportOutput#endRow()}</li>
+ *  </ol>
+ *  inside these methods you can always use the {@link #getOutputWriter()} method for accessing
+ *  the writer
+ * </p>
+ * 
+ * @author dragos balan (dragos dot balan @ gmail dot com)
+ * @since 0.7
+ * @see {@link AbstractByteOutput} {@link TxtOutput} {@link HtmlOutput} {@link StaxReportOutput}
  */
 public abstract class AbstractCharacterOutput extends AbstractOutput {
 	
@@ -76,5 +99,4 @@ public abstract class AbstractCharacterOutput extends AbstractOutput {
 	public void setFilePath(String filePath) {
 		setOutputWriter(ReportIoUtils.createWriterFromPath(filePath));
 	}
-
 }
