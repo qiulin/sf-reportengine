164f4fd1a0b470d1f0efb7f94381c8cab580d01c
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2013-02-19 17:44:04 +0000

    sql sample changed

--- reportengine-samples/src/main/java/net/sf/reportengine/samples/flat/FlatReportFromSqlQuery.java
+++ reportengine-samples/src/main/java/net/sf/reportengine/samples/flat/FlatReportFromSqlQuery.java
@@ -9,6 +9,7 @@ import net.sf.reportengine.in.DbQueryInput;
 import net.sf.reportengine.out.HtmlOutput;
 
 /**
+ * Normal report having as input an SQL query
  * @author dragos balan
  *
  */
@@ -18,20 +19,21 @@ public class FlatReportFromSqlQuery {
 	public static void main(String[] args) {
 		
 		FlatReport report = new FlatReport(); 
+		report.setReportTitle("Report obtained from SQL query"); 
 		
 		//input configuration
 		DbQueryInput reportInput = new DbQueryInput(); 
 		reportInput.setDbConnString("jdbc:oracle:thin:@172.21.45.35:1521:icisdev");
 		reportInput.setDbDriverClass("oracle.jdbc.driver.OracleDriver"); 
-		reportInput.setDbUser("scott");
-		reportInput.setDbPassword("tiger"); 
-		reportInput.setSqlStatement("select id, firstName, lastName , salary from emp");
+		reportInput.setDbUser("AIS_DEV");
+		reportInput.setDbPassword("AIS_DEV"); 
+		reportInput.setSqlStatement("select atl_id, atl_declaration_id, atl_declaration_type , atl_quantity from at_logs order by atl_id");
 		report.setIn(reportInput);
 		
-		report.addDataColumn(new DefaultDataColumn("id",0));
-		report.addDataColumn(new DefaultDataColumn("FirstName", 3)); 
-		report.addDataColumn(new DefaultDataColumn("LastName", 6));
-		report.addDataColumn(new DefaultDataColumn("Quantity", 10));
+		report.addDataColumn(new DefaultDataColumn("atl_id", 0));
+		report.addDataColumn(new DefaultDataColumn("atl_declaration_id", 1)); 
+		report.addDataColumn(new DefaultDataColumn("atl_declaration_type", 2));
+		report.addDataColumn(new DefaultDataColumn("atl_quantity", 3));
 				
 		report.setOut(new HtmlOutput("./output/ReportFromDbQuery.html"));
 		
