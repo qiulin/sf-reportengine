d94975b83f92146936d5261642b622da5a6011c7
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2013-05-06 20:19:07 +0000

    newRowComparator improved

--- reportengine/src/test/java/net/sf/reportengine/core/steps/TestNewRowComparator.java
+++ reportengine/src/test/java/net/sf/reportengine/core/steps/TestNewRowComparator.java
@@ -1,56 +1,75 @@
 package net.sf.reportengine.core.steps;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
 import net.sf.reportengine.core.algorithm.NewRowEvent;
-import net.sf.reportengine.scenarios.Scenario1;
+import net.sf.reportengine.scenarios.SortScenario;
 
 import org.junit.Test;
-import static org.junit.Assert.*; 
 
 public class TestNewRowComparator {
 
 	@Test
 	public void testCompare() {
-		NewRowComparator classUnderTest = new NewRowComparator(Scenario1.GROUPING_COLUMNS, Scenario1.DATA_COLUMNS);
-		
 		//first 3 cols are group cols, last 3 are data cols
-		NewRowEvent row1 = new NewRowEvent(new String[]{"1","2","3", "4","5","6"}); 
-		NewRowEvent row2 = new NewRowEvent(new String[]{"1","2","3", "3","3","3"}); 
+		//for data columns only the last two columns need ordering 
+		//the last one has a higher order priority than one before last
+		NewRowComparator classUnderTest = new NewRowComparator(
+													SortScenario.GROUPING_COLUMNS, 
+													SortScenario.DATA_COLUMNS);
+		
+		//same grouping columns but, the third data column has a diff value (3 vs 6)
+		NewRowEvent row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","3", "4","5","6"})); 
+		NewRowEvent row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","3", "3","3","3"})); 
 		assertTrue(classUnderTest.compare(row1, row2) > 0);
 		
-		row1 = new NewRowEvent(new String[]{"1","2","3", "4","5","6"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
+		//same grouping columns but, the second data column has a diff value (3 vs 5)
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "2","5","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "2","3","2"})); 
 		assertTrue(classUnderTest.compare(row1, row2) > 0);
 		
-		row1 = new NewRowEvent(new String[]{"1","2","2", "2","3","1"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
+		//same grouping columns but, the third data column has a diff value (1 vs 2)
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "1","1","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "1","3","1"})); 
 		assertTrue(classUnderTest.compare(row1, row2) > 0);
 		
-		row1 = new NewRowEvent(new String[]{"2","2","2", "4","5","6"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
+		//the third grouping column is different
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","3", "4","5","6"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "2","2","2"})); 
 		assertTrue(classUnderTest.compare(row1, row2) > 0);
 		
-		row1 = new NewRowEvent(new String[]{"1","2","2", "2","2","7"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
+		//first group column is different (1 vs 2)
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"2","2","2", "4","5","6"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "2","2","2"})); 
 		assertTrue(classUnderTest.compare(row1, row2) > 0);
 		
-		row1 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
+		//same values on the rows that matter ( the first data row doesn't matter)
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "1000","2","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "2000","2","2"})); 
 		assertEquals(0, classUnderTest.compare(row1, row2));
 		
-		row1 = new NewRowEvent(new String[]{"1","2","1", "2","2","2"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
-		assertEquals(-1, classUnderTest.compare(row1, row2));
 		
-		row1 = new NewRowEvent(new String[]{"1","1","1", "2","2","2"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
-		assertEquals(-1, classUnderTest.compare(row1, row2));
+		//the third group column is different
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","1", "2","2","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "2","2","2"})); 
+		assertTrue(classUnderTest.compare(row1, row2) < 0);
 		
-		row1 = new NewRowEvent(new String[]{"0","2","2", "2","2","2"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
-		assertEquals(-1, classUnderTest.compare(row1, row2));
+		//the second group column is different
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "2","2","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","2","2", "2","2","2"})); 
+		assertTrue(classUnderTest.compare(row1, row2) < 0);
 		
-		row1 = new NewRowEvent(new String[]{"1","2","2", "1","2","2"}); 
-		row2 = new NewRowEvent(new String[]{"1","2","2", "2","2","2"}); 
-		assertEquals(-1, classUnderTest.compare(row1, row2));
+		//the second data column is different
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "2","1","2"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "2","2","2"})); 
+		assertTrue(classUnderTest.compare(row1, row2) < 0);
+		
+		//
+		row1 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "1","1","1"})); 
+		row2 = new NewRowEvent(Arrays.asList(new Object[]{"1","1","1", "1","1","7"})); 
+		assertTrue(classUnderTest.compare(row1, row2) < 0);
 	}
 }
