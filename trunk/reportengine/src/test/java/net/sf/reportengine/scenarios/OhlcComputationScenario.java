5d89d706aeffbf3fca52864210c7f63d0145d254
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2014-02-08 12:58:54 +0000

    bug in UniversalMinCalculator fixed

--- trunk/reportengine/src/test/java/net/sf/reportengine/scenarios/OhlcComputationScenario.java
+++ trunk/reportengine/src/test/java/net/sf/reportengine/scenarios/OhlcComputationScenario.java
@@ -14,6 +14,7 @@ import net.sf.reportengine.core.calc.GroupCalculators;
 import net.sf.reportengine.core.calc.UniversalSumGroupCalculator;
 import net.sf.reportengine.in.ReportInput;
 import net.sf.reportengine.in.TextInput;
+import net.sf.reportengine.util.ReportIoUtils;
 
 /**
  * @author dragos balan
@@ -21,7 +22,7 @@ import net.sf.reportengine.in.TextInput;
  */
 public class OhlcComputationScenario {
 	
-	public static final ReportInput INPUT = new TextInput(OhlcComputationScenario.class.getClassLoader().getResourceAsStream(""),"\t");
+	public static final ReportInput INPUT = new TextInput(ReportIoUtils.createInputStreamFromClassPath("2010-1MIN-DATA.tsv"), "\t");
 	
 	public static final List<DataColumn> DATA_COLUMNS = Arrays.asList(
 	new DataColumn[]{
