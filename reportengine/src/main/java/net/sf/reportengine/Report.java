1fdd929f811cd06a0a787c5abcc4f95ae8dfd8c6
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2014-09-24 16:02:47 +0000

    Report component interface added

---
+++ reportengine/src/main/java/net/sf/reportengine/Report.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package net.sf.reportengine;
+
+/**
+ * Report interface 
+ * 
+ * @author dragos balan
+ * @since 0.13.0
+ */
+public interface Report {
+	
+	/**
+	 * adds a new component to the report
+	 * @param reportComponent	the component
+	 */
+	public void add(ReportComponent reportComponent); 
+	
+	/**
+	 * executes the report
+	 */
+	public void execute(); 
+}
---
+++ reportengine/src/main/java/net/sf/reportengine/ReportComponent.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package net.sf.reportengine;
+
+/**
+ * @author balan
+ *
+ */
+public interface ReportComponent {
+	public void output(); 
+}
