48899e1962062c042b308154f886cbbe096ecf96
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-06-17 19:51:11 +0000

    cosmetics after license text

--- reportengine/src/main/java/net/sf/reportengine/components/AbstractTable.java
+++ reportengine/src/main/java/net/sf/reportengine/components/AbstractTable.java
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * 
- */
 package net.sf.reportengine.components;
 
 import net.sf.reportengine.core.ConfigValidationException;
