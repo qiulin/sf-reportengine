8fcb599d01ca20c4daa6f7aaad4d39a89452f870
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-07-26 20:03:20 +0000

    lost files recovered

--- trunk/reportengine/src/test/java/net/sf/reportengine/out/neo/TestHtmlReportOutput.java
+++ trunk/reportengine/src/test/java/net/sf/reportengine/out/neo/TestHtmlReportOutput.java
@@ -1,5 +1,17 @@
 /**
- * copyright 2015 dragos balan
+ * Copyright (C) 2006 Dragos Balan (dragos.balan@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.sf.reportengine.out.neo;
 
@@ -10,6 +22,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+
+import net.sf.reportengine.out.TitleProps;
+import net.sf.reportengine.util.ReportIoUtils;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
@@ -70,5 +85,23 @@ public class TestHtmlReportOutput {
 		assertEquals("<br/>", lines.get(0));
 		assertEquals("<br/>", lines.get(1));
 		assertEquals("this text has been added after HtmlReportOutput has been closed", lines.get(2)); 
+	}
+	
+	
+	@Test
+	public void testUtf8Characters() throws IOException {
+		String OUTPUT_PATH = "target/TestHtmlReportOutputUtf8.html";
+		HtmlReportOutput testOutput = new HtmlReportOutput(
+				ReportIoUtils.createWriterFromPath(OUTPUT_PATH)); 
+		testOutput.open();
+		testOutput.output("title.ftl", new TitleProps("На берегу пустынных волн")); 
+		testOutput.output("title.ftl", new TitleProps("Τη γλώσσα μου έδωσαν ελληνική"));
+		testOutput.close(); 
+		
+		List<String> lines = IOUtils.readLines(ReportIoUtils.createReaderFromPath(OUTPUT_PATH)); 
+		assertEquals(2, lines.size()); 
+		assertEquals("<p style=\"text-align: center; vertical-align: middle\">На берегу пустынных волн</p>", lines.get(0));
+		assertEquals("<p style=\"text-align: center; vertical-align: middle\">Τη γλώσσα μου έδωσαν ελληνική</p>", lines.get(1)); 
+		
 	}
 }
