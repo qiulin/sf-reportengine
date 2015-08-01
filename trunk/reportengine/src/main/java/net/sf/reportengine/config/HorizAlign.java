d4f21b8202f72911628c0c7a589ec9868aa5253e
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-08-01 16:08:36 +0000

    poi and stax dependencies removed

--- trunk/reportengine/src/main/java/net/sf/reportengine/config/HorizAlign.java
+++ trunk/reportengine/src/main/java/net/sf/reportengine/config/HorizAlign.java
@@ -15,7 +15,6 @@
  */
 package net.sf.reportengine.config;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 
 
 /**
@@ -25,18 +24,18 @@ import org.apache.poi.hssf.usermodel.HSSFCellStyle;
  * @since 0.6
  */
 public enum HorizAlign {
-	LEFT("left", "left", HSSFCellStyle.ALIGN_LEFT), 
-	CENTER("center", "center", HSSFCellStyle.ALIGN_CENTER), 
-	RIGHT("right", "right", HSSFCellStyle.ALIGN_RIGHT); 
+	LEFT("left", "left"/*, HSSFCellStyle.ALIGN_LEFT*/), 
+	CENTER("center", "center"/*, HSSFCellStyle.ALIGN_CENTER*/), 
+	RIGHT("right", "right"/*, HSSFCellStyle.ALIGN_RIGHT*/); 
 	
 	private String htmlCode; 
 	private String foCode; 
-	private short hssfCode; 
+	//private short hssfCode; 
 	
-	private HorizAlign(String htmlCode, String foCode, short hssfCode){
+	private HorizAlign(String htmlCode, String foCode/*, short hssfCode*/){
 		this.htmlCode = htmlCode; 
 		this.foCode = foCode; 
-		this.hssfCode = hssfCode; 
+		//this.hssfCode = hssfCode; 
 	}
 	
 	public String getHtmlCode(){
@@ -47,7 +46,7 @@ public enum HorizAlign {
 		return foCode; 
 	}
 	
-	public short getHssfCode(){
-		return hssfCode; 
-	}
+//	public short getHssfCode(){
+//		return hssfCode; 
+//	}
 }
