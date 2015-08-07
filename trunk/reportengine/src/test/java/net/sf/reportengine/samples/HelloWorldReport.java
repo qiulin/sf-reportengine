616f808e987d526558855f0a3c25a64ca539383e
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-08-07 12:39:55 +0000

    samples added

---
+++ trunk/reportengine/src/test/java/net/sf/reportengine/samples/HelloWorldReport.java
@@ -0,0 +1,27 @@
+/**
+ * copyright 2015 dragos balan
+ */
+package net.sf.reportengine.samples;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import net.sf.reportengine.Report;
+import net.sf.reportengine.components.ReportTitle;
+import net.sf.reportengine.out.HtmlReportOutput;
+
+public class HelloWorldReport {
+
+  public static void main(String[] args) throws IOException{
+	//preparation of output and components
+	HtmlReportOutput output = new HtmlReportOutput(new FileWriter("Hello.html"));
+	ReportTitle title = new ReportTitle("Hello World report"); 
+
+	//report set-up
+	Report report  = new Report.Builder(output)
+						.add(title)
+						.build(); 
+	//report execution
+	report.execute(); 
+  }
+}
