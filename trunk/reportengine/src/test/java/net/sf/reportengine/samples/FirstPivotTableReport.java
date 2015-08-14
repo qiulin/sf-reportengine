8b68063eb117d7f9b10eef8d70cdce6bd1a79ba9
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-08-14 14:26:10 +0000

    PivotTableBuilder added

--- trunk/reportengine/src/test/java/net/sf/reportengine/samples/FirstPivotTableReport.java
+++ trunk/reportengine/src/test/java/net/sf/reportengine/samples/FirstPivotTableReport.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 import net.sf.reportengine.ReportBuilder;
 import net.sf.reportengine.components.PivotTable;
+import net.sf.reportengine.components.PivotTableBuilder;
 import net.sf.reportengine.components.ReportTitle;
 import net.sf.reportengine.config.DefaultDataColumn;
 import net.sf.reportengine.config.DefaultPivotData;
@@ -20,19 +21,21 @@ import net.sf.reportengine.out.HtmlReportOutput;
  */
 public class FirstPivotTableReport {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		PivotTable table = new PivotTable.Builder()
-				.input(new TextTableInput("./input/expenses.csv", ","))
-				.addDataColumn(new DefaultDataColumn("Month", 0))
-				.addHeaderRow(new DefaultPivotHeaderRow(1))
-				.pivotData(new DefaultPivotData(2)).build();
+        PivotTable table =
+            new PivotTableBuilder().input(new TextTableInput("./input/expenses.csv",
+                                                             ","))
+                                   .addDataColumn(new DefaultDataColumn("Month",
+                                                                        0))
+                                   .addHeaderRow(new DefaultPivotHeaderRow(1))
+                                   .pivotData(new DefaultPivotData(2))
+                                   .build();
 
-		new ReportBuilder(new HtmlReportOutput(new FileWriter(
-				"./target/ExpensesPivot.html")))
-				.add(new ReportTitle(
-						"This is my first report with a pivot table"))
-				.add(table).build().execute();
+        new ReportBuilder(new HtmlReportOutput(new FileWriter("./target/ExpensesPivot.html"))).add(new ReportTitle("This is my first report with a pivot table"))
+                                                                                              .add(table)
+                                                                                              .build()
+                                                                                              .execute();
 
-	}
+    }
 }
