b06945932cb3066124a663c340189ce3cd3c1a32
dragoshbalan <dragoshbalan@7d53476a-ff8d-4422-942d-4f2ff7e288af>
2015-08-21 19:36:51 +0000

    deleted the resource neo package

--- reportengine/src/main/java/net/sf/reportengine/out/PdfReportOutput.java
+++ reportengine/src/main/java/net/sf/reportengine/out/PdfReportOutput.java
@@ -23,17 +23,18 @@ import java.io.OutputStream;
 import org.apache.xmlgraphics.util.MimeConstants;
 
 /**
+ * PDF Output for reports.
+ * 
  * @author dragos balan
  *
  */
 public class PdfReportOutput extends PostProcessedFoReportOutput {
-	
-	
-	public PdfReportOutput(OutputStream outStream){
-		this(outStream, new PdfOutputFormat()); 
-	}
-	
-	public PdfReportOutput(OutputStream outStream, PdfOutputFormat outputFormat){
-		super(outStream, outputFormat, new FopTransformerPostProcessor(MimeConstants.MIME_PDF)); 
-	}
+
+    public PdfReportOutput(OutputStream outStream) {
+        this(outStream, new PdfOutputFormat());
+    }
+
+    public PdfReportOutput(OutputStream outStream, PdfOutputFormat outputFormat) {
+        super(outStream, outputFormat, new FopTransformerPostProcessor(MimeConstants.MIME_PDF));
+    }
 }
